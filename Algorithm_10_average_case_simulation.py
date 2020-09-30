2538554527e50b030c33eb12b714fa3aa20c93cb
floratheeexplorer <51019057+floratheeexplorer@users.noreply.github.com>
2020-09-30 12:30:13 +0200

    Add files via upload

--- Algorithm_10_average_case_simulation.py
+++ Algorithm_10_average_case_simulation.py
@@ -4,178 +4,165 @@
 
 Created on Thu Dec 12 16:08:11 2019
 
- 
-
-@author: SVISAGIE
 
 """
-
  
 
-#Hardloop 'n klomp simulasies om die verskil tussen z en u te meet
+#simulations to measure the difference between z and u
 
  
-
 import random
 
  
+def u_vs_z(no_bays,no_prod,gap):
 
-def u_vs_z(aant_bays,aant_prod,gap):
+    #no_bays = 60 #length of line
 
-    #aant_bays = 60 #length of line
-
-    #aant_prod = 3 #number of SKUs
+    #no_prod = 3 #number of SKUs
 
     #gap = 3 #length of gap
 
-    #file_naam = str(aant_bays) + '_' + str(aant_prod)
+    #file_name = str(no_bays) + '_' + str(no_prod)
 
    
 
-    prod_plek = [i for i in range(aant_prod)]
+    prod_place = [i for i in range(no_prod)]
 
-    rand_nums = [i for i in range(aant_bays)]
+    rand_num = [i for i in range(no_bays)]
 
-    lys_bo = []
+    list_above = []
 
-    lys_onder = []
+    list_under = []
 
-    random.shuffle(rand_nums)
+    random.shuffle(rand_num)
+  
 
-   
+    for j in range(no_bays):
 
-    for j in range(aant_bays):
+        if rand_num[j] < no_prod:
 
-        if rand_nums[j] < aant_prod:
+            prod_place[rand_num[j]] = j          
+  
 
-            prod_plek[rand_nums[j]] = j
+    #calculate distance
 
-           
+    prod_in_follow = sorted(prod_place)
 
-    
+    #print(prod_in_follow)  
 
-    #bereken u afstand
+    space = 0
 
-    prod_in_volg = sorted(prod_plek)
-
-    #print(prod_in_volg)
-
-   
-
-    gat = 0
-
-    beste_gat = 0
+    best_space = 0
 
        
 
-    for i in range(aant_prod):
+    for i in range(no_prod):
 
-        gat = (aant_bays + prod_in_volg[i] - prod_in_volg[i-1]) % aant_bays
+        space = (no_bays + prod_in_follow[i] - prod_in_follow[i-1]) % no_bays
 
-        if gat > beste_gat:
+        if space > best_space:
 
-            beste_gat = gat
-
-   
-
-    u_pick_lengte = aant_bays - beste_gat + 1
+            best_space = space
 
    
 
-    for i in range (aant_prod):
+    u_pick_length = no_bays - best_space + 1
 
-        if prod_in_volg[i] <= aant_bays/2:
+   
 
-            lys_bo.append(prod_in_volg[i])
+    for i in range (no_prod):
+
+        if prod_in_follow[i] <= no_bays/2:
+
+            list_above.append(prod_in_follow[i])
 
         else:
 
-            lys_onder.insert(0,aant_bays - prod_in_volg[i])
+            list_under.insert(0, no_bays - prod_in_follow[i])
 
    
 
-    lys_bo.append(1000)
+    list_above.append(1000)
 
-    lys_onder.append(1000)
+    list_under.append(1000)
+
+   
+    #print(list_above)
+
+    #print(lys_under)
+
+    z_pick_length = 0
 
    
 
-    #print(lys_bo)
+    if list_above[0] <= list_under[0]:
 
-    #print(lys_onder)
+        is_above = True
 
-    z_pick_lengte = 0
-
-   
-
-    if lys_bo[0] <= lys_onder[0]:
-
-        is_bo = True
-
-        vorige_stop = lys_bo.pop(0)
+        previous_stop = list_above.pop(0)
 
     else:
 
-        is_bo = False
+        is_above = False
 
-        vorige_stop = lys_onder.pop(0)
+        previous_stop = list_under.pop(0)
 
    
 
-    #print(lys_bo)
+    #print(list_above)
 
-    #print(lys_onder)
+    #print(list_under)
    
 
-    for i in range(aant_prod-1):
+    for i in range(no_prod-1):
 
-        if lys_bo[0] <= lys_onder[0]:
+        if list_above[0] <= list_under[0]:
 
-            if is_bo == True:
+            if is_above == True:
 
-                z_stap_lengte = lys_bo[0] - vorige_stop
+                z_step_length = list_above[0] - previous_stop
 
-                vorige_stop = lys_bo.pop(0)
+                previous_stop = list_above.pop(0)
 
             else:
 
-                z_stap_lengte = lys_bo[0] - vorige_stop + gap
+                z_step_length = list_above[0] - previous_stop + gap
 
-                is_bo = True
+                is_above = True
 
-                vorige_stop = lys_bo.pop(0)
+                previous_stop = list_above.pop(0)
 
         else:
 
-            if is_bo == True:
+            if is_above == True:
 
-                z_stap_lengte = lys_onder[0] - vorige_stop + gap
+                z_step_length = list_under[0] - previous_stop + gap
 
-                is_bo = False
+                is_above = False
 
-                vorige_stop = lys_onder.pop(0)
+                previous_stop = list_under.pop(0)
 
             else:
 
-                z_stap_lengte = lys_onder[0] - vorige_stop
+                z_step_length = list_under[0] - previous_stop
 
-                vorige_stop = lys_onder.pop(0)
+                previous_stop = list_under.pop(0)
 
-        z_pick_lengte = z_pick_lengte + z_stap_lengte
+        z_pick_length = z_pick_length + z_step_length
 
-        #print(z_stap_lengte)
+        #print(z_step_length)
 
-        #print(z_pick_lengte)
+        #print(z_pick_length)
 
-        #print(lys_bo)
+        #print(list_above)
 
-        #print(lys_onder)
+        #print(list_under)
 
-    return u_pick_lengte - z_pick_lengte
+    return u_pick_length - z_pick_length
 
-    #print(str(u_pick_lengte) + ' ' + str(z_pick_lengte))
+    #print(str(u_pick_length) + ' ' + str(z_pick_length))
 
-    #print(u_pick_lengte - z_pick_lengte)
+    #print(u_pick_length - z_pick_length)
 
 for k in range(29): #number of SKUs
 
